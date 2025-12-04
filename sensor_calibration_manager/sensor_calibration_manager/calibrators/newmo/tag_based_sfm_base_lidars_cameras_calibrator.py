b18a5c77d9c268ee397a9a79563768db30c45fa4
genkey6 <gen.shu@newmo.me>
2025-12-04 14:54:03 +0900

    feat: improve tag_based_sfm_base_lidars_cameras_calibrator

--- sensor_calibration_manager/sensor_calibration_manager/calibrators/newmo/tag_based_sfm_base_lidars_cameras_calibrator.py
+++ sensor_calibration_manager/sensor_calibration_manager/calibrators/newmo/tag_based_sfm_base_lidars_cameras_calibrator.py
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Dict
+
+import numpy as np
+
 from sensor_calibration_manager.calibrator_base import CalibratorBase
 from sensor_calibration_manager.calibrator_registry import CalibratorRegistry
 from sensor_calibration_manager.ros_interface import RosInterface
@@ -30,12 +34,16 @@ class TagBasedSfmBaseLidarsCamerasCalibrator(CalibratorBase):
         super().__init__(ros_interface)
 
         self.base_frame = kwargs["base_frame"]
+        self.sensor_kit_frame = "sensor_kit_base_link"
 
         self.main_sensor_frame = kwargs["main_calibration_sensor_frame"]
         self.calibration_lidar_frames = [
             kwargs["calibration_lidar_1_frame"],
             kwargs["calibration_lidar_2_frame"],
             kwargs["calibration_lidar_3_frame"],
+        ]
+        self.calibration_lidar_base_frames = [
+            lidar_frame + "_base_link" for lidar_frame in self.calibration_lidar_frames
         ]
 
         self.calibration_camera_optical_link_frames = [
@@ -47,13 +55,19 @@ class TagBasedSfmBaseLidarsCamerasCalibrator(CalibratorBase):
             kwargs["calibration_camera_5_frame"],
             kwargs["calibration_camera_6_frame"],
         ]
+        self.calibration_camera_link_frames = [
+            camera_frame.replace("camera_optical_link", "camera_link")
+            for camera_frame in self.calibration_camera_optical_link_frames
+        ]
 
         self.required_frames.extend(
             [
                 self.base_frame,
+                self.sensor_kit_frame,
                 self.main_sensor_frame,
                 *self.calibration_lidar_frames,
                 *self.calibration_camera_optical_link_frames,
+                *self.calibration_camera_link_frames,
             ]
         )
 
@@ -71,3 +85,45 @@ class TagBasedSfmBaseLidarsCamerasCalibrator(CalibratorBase):
                 ],
             ],
         )
+
+    def post_process(self, calibration_transforms: Dict[str, Dict[str, np.array]]):
+        sensor_kit_to_mapping_lidar_transform = self.get_transform_matrix(
+            self.sensor_kit_frame, self.main_sensor_frame
+        )
+
+        optical_link_to_camera_link_transforms = [
+            self.get_transform_matrix(camera_optical_link_frame, camera_link_frame)
+            for camera_optical_link_frame, camera_link_frame in zip(
+                self.calibration_camera_optical_link_frames, self.calibration_camera_link_frames
+            )
+        ]
+
+        base_to_top_sensor_kit_transform = np.linalg.inv(
+            sensor_kit_to_mapping_lidar_transform
+            @ calibration_transforms[self.main_sensor_frame][self.base_frame]
+        )
+        results = {self.base_frame: {self.sensor_kit_frame: base_to_top_sensor_kit_transform}}
+        results[self.sensor_kit_frame] = {}
+
+        results[self.sensor_kit_frame][self.main_sensor_frame] = (
+            sensor_kit_to_mapping_lidar_transform
+        )
+
+        for lidar_frame in self.calibration_lidar_frames:
+            results[self.sensor_kit_frame][lidar_frame] = (
+                sensor_kit_to_mapping_lidar_transform
+                @ calibration_transforms[self.main_sensor_frame][lidar_frame]
+            )
+
+        for camera_frame, optical_link_to_camera_link_transform in zip(
+            self.calibration_camera_optical_link_frames, optical_link_to_camera_link_transforms
+        ):
+            results[self.sensor_kit_frame][
+                camera_frame.replace("camera_optical_link", "camera_link")
+            ] = (
+                sensor_kit_to_mapping_lidar_transform
+                @ calibration_transforms[self.main_sensor_frame][camera_frame]
+                @ optical_link_to_camera_link_transform
+            )
+
+        return results
